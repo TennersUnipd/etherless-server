e5b56d538d91db15f16a88579e3903458610a579
Gianmarco Pettinato <gianmarco.pettinato@gmail.com>
2020-02-29 21:22:05 +0100

    updated configuration

--- src/configurator.ts
+++ src/configurator.ts
@@ -8,9 +8,9 @@ export enum EnvType {
 export function getConfiguration(forEnv: EnvType): GatewayConfiguration {
     switch (forEnv) {
         case EnvType.Local: return {
-            providerURI: process.env.WEBSOCKET_PROVIDER ? process.env.WEBSOCKET_PROVIDER : "ws://127.0.0.1:7545",
+            providerURI: process.env.WEBSOCKET_PROVIDER ? process.env.WEBSOCKET_PROVIDER : "ws://taverna.pettinato.eu:7545",
             abiFile: "./contracts/EtherlessSmart.json",
-            contractAddress: process.env.CONTRACT_ADDRESS ? process.env.CONTRACT_ADDRESS : "0x9EfbEd20Fd55012CFDbD0A4400068ce6764A95E7"
+            contractAddress: process.env.CONTRACT_ADDRESS ? process.env.CONTRACT_ADDRESS : "0xFa34Ae983f823C5e95c9A2E0ec164F1813F187d7"
         }
         case EnvType.Test: return {
             providerURI: process.env.WEBSOCKET_PROVIDER ? process.env.WEBSOCKET_PROVIDER : "",
