29282b0ab426c4a11a17c7686ca1a63094980b18
Gianmarco Pettinato <gianmarco.pettinato@gmail.com>
2020-02-29 17:46:04 +0100

    POC aws funzionante per l'upload delle funzioni

--- handler.ts
+++ handler.ts
@@ -1,59 +1,41 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-const multipart = require('aws-lambda-multipart-parser')
-import 'source-map-support/register';
-var AWS = require('aws-sdk');
-var lambda = new AWS.Lambda({ region: 'us-east-1' })
 
-const runtime: string = "nodejs12.x";
-const ARN_ROLE: string = "arn:aws:iam::111040565648:role/LambdaDeployer";
+const AWS = require('aws-sdk');
+
+const lambda = new AWS.Lambda({ region: 'us-east-1' });
+
+const runtime: string = 'nodejs12.x';
+const ARN_ROLE: string = 'arn:aws:iam::964189167587:role/etherless-dev';
 const FN_TIMEOUT = 30;
 
-interface retriveFunctionData {
-  name: string,
-  proto: JSON
-}
-
-export const createFunction: APIGatewayProxyHandler = async (event, _context) => {
-  let data = multipart.parse(event);
-  let ZipFile = data.zip.content;
-  let dataFunction = JSON.parse(data.request.content);
-  let functionToStore = {
+export const createFunction:APIGatewayProxyHandler = async (event) => {
+  const data = JSON.parse(event.body);
+  const buffer = Buffer.from(data.zip, 'utf8');
+  const functionToStore = {
     Code: {
-      ZipFile: ZipFile
+      ZipFile: buffer,
     },
-    Description: dataFunction.description,
-    FunctionName: dataFunction.name,
-    Handler: dataFunction.name + ".handler",
+    FunctionName: data.name,
+    Handler: `${data.name}.handler`,
     MemorySize: 128,
     Publish: true,
     Role: ARN_ROLE,
     Runtime: runtime,
     Timeout: FN_TIMEOUT,
     VpcConfig: {
-    }
+    },
   };
-
-  let prom = new Promise((resolve, reject) => {
+  const prom = new Promise((resolve, reject) => {
     lambda.createFunction(functionToStore, (err: any, rData: any) => {
       if (err) reject(err);
       else resolve(rData);
     });
-  })
-  let response = await prom;
-  console.log(prom.resolve);
-  console.log(prom.reject);
+  });
+  const response = await prom;
+  if (response) {
+    return { statusCode: 200, body: 'OK' };
+  }
+  return { statusCode: 500, body: 'wrong request' };
+};
 
-  return {
-    statusCode: 200,
-    body: "OK"
-  };
-}
-
-export const retriveFunction: APIGatewayProxyHandler = async (event, _context) => {
-  let request: retriveFunctionData = JSON.parse(JSON.stringify(event)).body;
-  console.log(request);
-  return {
-    statusCode: 200,
-    body: "OK"
-  };
-}
+export default 'createFunction';
