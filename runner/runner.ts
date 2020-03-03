ae903a2275ee9fe5111c0ba10f23e7e730dc9225
Gianmarco Pettinato <gianmarco.pettinato@gmail.com>
2020-03-03 11:04:12 +0100

    refactoring eslint

--- runner/runner.ts
+++ runner/runner.ts
@@ -1,52 +1,54 @@
-require('dotenv').config()
-import { getConfiguration, EnvType } from "./configurator"
-import { Gateway } from "./gateway"
+import { getConfiguration, EnvType } from './configurator';
+import { Gateway } from './gateway';
+
+require('dotenv').config();
 
 // Listens for eth events, calls lambda functions, returns output to etherless-cli
 
 // configure AWS Lambda service (uses credentials from ~/.aws/credentials)
-var AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 // AWS.config.loadFromPath('./aws-credentials.json')
-var lambda = new AWS.Lambda({ region: 'us-east-1' })
+const lambda = new AWS.Lambda({ region: 'us-east-1' });
 
-let gatewayConfig = getConfiguration(EnvType.Test)
-let gateway = new Gateway(gatewayConfig)
-
-var requestsCounter = 0;
-console.log("Started listening for execution requests...");
-gateway.contract.events.RemoteExec(function (error, event){
-    console.log("Received request #"+(++requestsCounter))
-    if (error == null) {
-        // get data from event
-        let functionRemoteResource = event.returnValues["_name"];
-        let functionParameters = event.returnValues["_parameters"];
-        let identifier = event.returnValues["_identifier"];
-        // TODO: check prev values content
-
-        console.log("Call to function " + functionRemoteResource + " with params "+ functionParameters);
-        executeLambdaFunction(functionRemoteResource, functionParameters).then((response) => {
-            console.log(response);
-            console.log("Now giving back to etherless-cli");
-            let jsonResponse = JSON.stringify(response);
-            gateway.contract.methods.execRemoteResponse(jsonResponse, identifier).send({from: gateway.testAccount}).catch(console.error);
-        }).catch(console.error)
-    } else {
-        console.error("An error occured", error);
-    }
-});
+const gatewayConfig = getConfiguration(EnvType.Test);
+const gateway = new Gateway(gatewayConfig);
 
 function executeLambdaFunction(arn, params) {
-    var functionDescriptor = {
-        FunctionName: arn,
-        Payload: params
-       };
-    return lambda.invoke(functionDescriptor).promise();
+  const functionDescriptor = {
+    FunctionName: arn,
+    Payload: params,
+  };
+  return lambda.invoke(functionDescriptor).promise();
 }
 
-process.on("exit", function(){
-    console.log("Close connection to network")
-    gateway.disconnect()
-})
-process.on('SIGINT', function () {
-    process.exit()
-})
+let requestsCounter = 0;
+console.log('Started listening for execution requests...');
+gateway.contract.events.RemoteExec((error, event) => {
+  console.log(`Received request #${++requestsCounter}`);
+  if (error == null) {
+    // get data from event
+    const functionRemoteResource = event.returnValues._name;
+    const functionParameters = event.returnValues._parameters;
+    const identifier = event.returnValues._identifier;
+    // TODO: check prev values content
+
+    console.log(`Call to function ${functionRemoteResource} with params ${functionParameters}`);
+    executeLambdaFunction(functionRemoteResource, functionParameters).then((response) => {
+      console.log(response);
+      console.log('Now giving back to etherless-cli');
+      const jsonResponse = JSON.stringify(response);
+      gateway.contract.methods.execRemoteResponse(jsonResponse, identifier)
+        .send({ from: gateway.testAccount }).catch(console.error);
+    }).catch(console.error);
+  } else {
+    console.error('An error occured', error);
+  }
+});
+
+process.on('exit', () => {
+  console.log('Close connection to network');
+  gateway.disconnect();
+});
+process.on('SIGINT', () => {
+  process.exit();
+});
