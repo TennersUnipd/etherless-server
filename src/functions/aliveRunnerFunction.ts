f73f17f85ae29e5f60a2976b4d969d7af7ed953a
Gabriel Ciulei <ciulei.gabriel@gmail.com>
2020-03-31 19:48:15 +0200

    compiling typescript

--- src/functions/aliveRunnerFunction.ts
+++
@@ -1,9 +0,0 @@
-const aliveRunner = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Runner running'),
-  };
-  return response;
-};
-
-export default aliveRunner;
