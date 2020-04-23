3dc586c714d0d38fd6e672a2e7b51ad1a12e0983
nicola <nicola.salvadore@gmail.com>
2020-04-23 18:00:56 +0200

    implementata prima versione

---
+++ src/functions/updateFunction.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import AWSInstance from '../awsInstance';
+
+
+export class FunctionDeployer{
+  private aws:AWSInstance;
+
+  constructor(aws:AWSInstance){
+    this.aws = aws;
+  }
+
+  public prepareFunctionToUpdate(parsedData:any):any {
+   return {
+      FunctionName: parsedData.ARN,
+      ZipFile: Buffer.from(parsedData.zip, 'utf8'),
+      Publish: true,
+    }
+  }
+
+  public letUpdateFunction(functionDefinition):Promise<any>{
+    return new Promise((resolve,reject)=>{
+
+      this.aws.getLambda().updateFunctionCode(functionDefinition, (err:any,rData) =>{
+        if(err) reject(err);
+        else resolve(rData);
+      })
+    });
+  }
+}
+
+export const updateFunction:APIGatewayProxyHandler = async (event) => {
+  const deployer:FunctionDeployer = new FunctionDeployer(new AWSInstance());
+  const data = JSON.parse(event.body);
+  let functionSerialized = this.aws.prepareFunctionToUpdate(data);
+  const prom = deployer.letUpdateFunction(functionSerialized);
+  let ARN = '';
+  await prom.then((result) => {
+    ARN = (JSON.stringify(result));
+  });
+  return { statusCode: 200, body: ARN };
+};
+
+export default createFunction;
