6e885049f2d9f4ec52fa7920778c9c4615a946d6
nicola <nicola.salvadore@gmail.com>
2020-04-24 17:55:46 +0200

    test implemented

--- tests/deleteFunctionTest.ts
+++ tests/deleteFunctionTest.ts
@@ -1,4 +1,4 @@
-import {FunctionDeployer} from  '../src/functions/createFunction'
+import {FunctionDeployer} from  '../src/functions/deleteFunction'
 import {describe, it} from 'mocha'
 import mockito from 'ts-mockito'
 import {assert} from 'chai'
@@ -25,32 +25,21 @@ describe('testing FunctionDeployer', () => {
     // STUB FOR AWSInstance Methods
     mockito.when(mockedAWS.getLambda()).thenReturn(new AWS.Lambda({ region: 'us-east-1' })); 
     const data = {name:'testName',zip:zipBuffer};
-    mockito.when(mockedAWS.prepareFunctionToStore(data)).thenReturn(
+    mockito.when(mockedAWS.prepareFunctionToDelete(data)).thenReturn(
       {
-        Code: {
-          ZipFile: zipBuffer,
-        },
         FunctionName: 'testName',
-        Handler: 'testName.handler',
-        MemorySize: 128,
-        Publish: true,
-        Role: 'roleString',
-        Runtime: 'runTimeString',
-        Timeout: 10,
-        VpcConfig: {
-        },
       }
     )
-    it('testing uploadFunction()',()=>{
-        AWSMock.mock('Lambda','createFunction',(params: GetItemInput, callBack: Function)=>{
+    it('testing deleteFunction()',()=>{
+        AWSMock.mock('Lambda','deleteFunction',(params: GetItemInput, callBack: Function)=>{
             console.log(params);
             console.log('calledMocked');
             callBack(null,{arnFunction:'test'});
         });
         console.log(data);
-        const toLoad = mockedAWS.prepareFunctionToStore(data);
-        console.log(toLoad);
-        fnDeployer.uploadFunction(toLoad)
+        const toDelete = mockedAWS.prepareFunctionToDelete(data);
+        console.log(toDelete);
+        fnDeployer.letFunctionDelete(toDelete)
           .then((result)=>{console.log(result)})
           .catch((err)=>{assert.fail(err)});
     });
